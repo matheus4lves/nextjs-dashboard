4f1f6dcd371b45defd49d795d580d469f29ccc40
Matheus Alves <matheusalves.bmd@gmail.com>
2024-06-11 16:18:56 -0300

    Add another custom font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
+
+const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+
+export { inter, lusitana };
